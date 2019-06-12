669809034ca140cca5d6a90418c3ef31696b6c25
nazzang49 <nazzang49@naver.com>
2019-06-12 14:54:59 +0900

    commit

--- int.py
+++ int.py
@@ -1,2 +1,26 @@
 a = 23
 print(a, type(a))
+# Integer 클래스 타입의 객체인지 확인
+print(isinstance(a,int))
+
+# 2진수
+b = 0b1101
+# 16진수
+c = 0o23
+# 8진수
+d = 0x23
+print(b,c,d)
+
+# int와 long 구분 X = 모두 int cf) 자바
+e = 2**1024
+print(e, type(e))
+
+# 변환변수 (10진수 -> n진수)
+print(oct(38))
+print(hex(38))
+print(bin(38))
+
+
+
+
+
